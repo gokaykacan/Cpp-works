5990c44a766aa51285fe1f481d696f170c18996d
Gökay <gokay147@gmail.com>
2019-02-01 16:25:23 +0300

    Functors section is modified

--- Functors/src/Functors.cpp
+++ Functors/src/Functors.cpp
@@ -6,10 +6,41 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+/*
+ * A functor (or function object) is a C++ class that acts like a function.
+ * Functors are called using the same old function call syntax. To create a functor,
+ * we create a object that overloads the operator(). ... Thus, an object a is created
+ * that overloads the operator().
+*/
 #include <iostream>
 using namespace std;
 
+struct Test{
+	virtual bool operator()(string &test)=0;
+	virtual ~Test(){}
+};
+struct Matchtest: public Test{
+	bool operator() (string &text){
+		return text == "lion";
+	}
+};
+
+void check(string text, Test &test){
+	if(test(text)){
+		cout << "String Matches !" << endl;
+	}
+	else{
+		cout << "String does not match !" << endl;
+	}
+}
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	Matchtest pred;
+
+	string value = "lion";
+
+	Matchtest t;
+	check("lion", t);
+
 	return 0;
 }
